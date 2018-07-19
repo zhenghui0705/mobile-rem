ce3312acdb28add472bcd3fcb657f8a4d9c8bbe4
zhenghui <hui@zhenghuideMacBook-Pro.local>
2018-07-19 12:11:26 +0800

    兼容老款浏览器

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('./index.scss');
 
 //获取html视图宽度
-let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+var htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
 // 获取html Dom
-let htmlDom = document.getElementsByTagName('html')[0]
+var htmlDom = document.getElementsByTagName('html')[0]
 // 设置html字体大小
 htmlDom.style.fontSize = htmlWidth / 20 + 'px';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,45 @@
 var webpack = require("webpack");
 var path = require('path');
 module.exports = {
-     //页面入口文件配置
-     entry: [
-         './app.js'
-     ],
-     //入口文件输出配置
-     output: {
-         path: path.resolve(__dirname,'./bulid'),
-         filename: 'bundle.js'
-     },
-     module:{
-         //加载器配置
-         loaders:[
-             {test:/\.css$/, loaders:"style-loader!css-loader"},
-             {test:/\.scss$/, loader:"style-loader!css-loader!sass-loader"},
-             {test: /\.(jpg|png|gif|svg)$/, use: ['url-loader?limit=8192&name=./[name].[ext]']},/*解析图片*/
-         ],
-     }
+    //页面入口文件配置
+    entry: [
+        './app.js'
+    ],
+    //入口文件输出配置
+    output: {
+        path: path.resolve(__dirname, './bulid'),
+        filename: 'bundle.js'
+    },
+    output: {
+        path: __dirname + '/dist/', //打包后的路径
+        filename: 'js/bundle.js'
+    },
+    module: {
+        //加载器配置
+        loaders: [{
+                test: /\.css$/,
+                loaders: "style-loader!css-loader"
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader"
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: ['url-loader?limit=8192&name=./[name].[ext]']
+            }, /*解析图片*/
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                //打包除这个文件之外的文件
+                exclude: path.resolve(__dirname, "./node_modules"),
+                //打包包括的文件
+                include: path.resolve(__dirname, "./*"),
+                options: {
+                    'presets': ['latest']
+                }
+            }
+        ],
+    },
+
 }
